17c780e9f54a7653c075bdb0d7bdf63b4bbfabac
Aslak Knutsen <aslak@4fs.no>
2019-08-21 13:09:53 +0200

    resolve gopath first

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,38 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
 async function run() {
   try {
-    core.addPath("$(go env GOPATH)/bin")
+
+    var goEnvPath = await getGoPath()
+    core.addPath(goEnvPath + "/bin")
     
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function getGoPath(): Promise<string> {
+  var goEnvPath = ""
+  var options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        goEnvPath += data.toString();
+      },
+      silent: true,    
+    }
+  }
+  var done =  exec.exec("go", ["env", "GOPATH"], options)
+  var prom = new Promise<string>((resolve, reject) => {
+    done.then(() => {
+      resolve(goEnvPath.replace(/\r?\n|\r/g, ""))
+    })
+    done.catch((res) => {
+      reject(res)
+    })
+  });
+
+  return prom
 }
 
 run();
